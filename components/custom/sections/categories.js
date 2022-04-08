d47260a51592b1034032f0d429727d2ea2be32bc
james <jamesdream0724@gmail.com>
2022-04-08 22:48:37 +0700

    stuck in build error :(

--- components/custom/sections/categories.js
+++ components/custom/sections/categories.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import Image from "next/image";
-import img1 from "../../../assets/images/categories/Medical.webp";
-import img2 from "../../../assets/images/categories/Memorial.webp";
-import img3 from "../../../assets/images/categories/Emergency.webp";
-import img4 from "../../../assets/images/categories/Nonprofit.webp";
-import img5 from "../../../assets/images/categories/Education.webp";
-import img6 from "../../../assets/images/categories/Animals.webp";
-import img7 from "../../../assets/images/categories/Environment.webp";
-import img8 from "../../../assets/images/categories/Business.webp";
-import img9 from "../../../assets/images/categories/Community.webp";
-import img10 from "../../../assets/images/categories/Charitytoken.webp";
-import img11 from "../../../assets/images/categories/Creative.webp";
-import img12 from "../../../assets/images/categories/Event.webp";
-import img13 from "../../../assets/images/categories/Faith.webp";
-import img14 from "../../../assets/images/categories/Family.webp";
-import img15 from "../../../assets/images/categories/Sports.webp";
-import img16 from "../../../assets/images/categories/Travel.webp";
-import img17 from "../../../assets/images/categories/Volunteer.webp";
-import img18 from "../../../assets/images/categories/Wishes.webp";
+import img1 from "../../../assets/images/categories/medical.webp";
+import img2 from "../../../assets/images/categories/memorial.webp";
+import img3 from "../../../assets/images/categories/emergency.webp";
+import img4 from "../../../assets/images/categories/nonprofit.webp";
+import img5 from "../../../assets/images/categories/education.webp";
+import img6 from "../../../assets/images/categories/animals.webp";
+import img7 from "../../../assets/images/categories/environment.webp";
+import img8 from "../../../assets/images/categories/business.webp";
+import img9 from "../../../assets/images/categories/community.webp";
+import img10 from "../../../assets/images/categories/charitytoken.webp";
+import img11 from "../../../assets/images/categories/creative.webp";
+import img12 from "../../../assets/images/categories/event.webp";
+import img13 from "../../../assets/images/categories/faith.webp";
+import img14 from "../../../assets/images/categories/family.webp";
+import img15 from "../../../assets/images/categories/sports.webp";
+import img16 from "../../../assets/images/categories/travel.webp";
+import img17 from "../../../assets/images/categories/volunteer.webp";
+import img18 from "../../../assets/images/categories/wishes.webp";
 
 const Categories = () => {
   return (
@@ -135,6 +135,7 @@ const Categories = () => {
             <Col lg="2" md="3" sm="6" xs="12" className="align-center p-5" >
                 <Image
                     src={img10}
+                    type="image/webp"
                     width={120}
                     height={120}
                     alt="icon-globe"
