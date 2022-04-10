6f6f7beb3346b8d02514cee38cd05291dab78f70
james <jamesdream0724@gmail.com>
2022-04-10 19:11:09 +0700

    fix build error

--- components/banner/Banner3.js
+++ components/banner/Banner3.js
@@ -12,7 +12,7 @@ const Banner3 = () => {
         <Row className="">
           <Col lg="6" md="6" className="align-self-center m-b-20">
             <h4 className="subcontent font-light" style={{lineHeight: 2}}>
-                Here at <b className="boldcontent">Sharity's Gift</b>, our mission is to make <b className="boldcontent">crypto giving simple</b> while highlighting your organization. We are creating a future where the gift of crypto donations becomes a standard. Our platform provides <b className="boldcontent">convenience</b> for the crypto donor, and a secure <b className="boldcontent">solution</b> for the charity, non profit, or organization to accept crypto donations <b className="boldcontent">safely.</b>
+                Here at <b className="boldcontent">Sharity&apos;s Gift</b>, our mission is to make <b className="boldcontent">crypto giving simple</b> while highlighting your organization. We are creating a future where the gift of crypto donations becomes a standard. Our platform provides <b className="boldcontent">convenience</b> for the crypto donor, and a secure <b className="boldcontent">solution</b> for the charity, non profit, or organization to accept crypto donations <b className="boldcontent">safely.</b>
             </h4>
             {/* <IconCard /> */}
             
