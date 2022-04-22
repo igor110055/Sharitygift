de1660d3553a5b3fec661e225d208fb55b2bcec1
jl <jamesdream0724@gmail.com>
2022-04-22 08:38:23 +0700

    take maximum 50 charities per api

--- pages/explorer.js
+++ pages/explorer.js
@@ -208,7 +208,7 @@ export async function getStaticProps() {
   ]
   let charities = {}
   for ( let i = 0; i < categories.length; i ++ ){
-    const charity = await fetch("https://partners.every.org/v0.2/browse/"+categories[i]+"?apiKey=72a0e0f6c64e2d13ee4108a39acfa99a");
+    const charity = await fetch("https://partners.every.org/v0.2/browse/"+categories[i]+"?apiKey=72a0e0f6c64e2d13ee4108a39acfa99a&take=50");
     const json = await charity.json()
     charities[categories[i]] = json['nonprofits']
   }
