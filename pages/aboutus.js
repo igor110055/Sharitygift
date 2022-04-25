dc29b8d10ab1fb38e073bac02a5a87e3b4fc0a78
jl <jamesdream0724@gmail.com>
2022-04-25 10:58:37 +0700

    add youtube --3

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -40,7 +40,7 @@ export default function Donors() {
                     <div className='player-wrapper'>
                       <ReactPlayer
                         className='react-player'
-                        url='https://youtu.be/JvALKuVfBzM'
+                        url='https://www.youtube.com/watch?v=JvALKuVfBzM'
                         width='100%'
                         height='100%'
                       />
